a0430972a8407b66a5426ce6c0e2e2f0bc482340
Sho Takahashi　 <sho.takahashi@yts-inc.net>
2021-01-30 21:07:59 +0900

    Manage order's status for the flow of order.
    https://youtu.be/2UqoE7Z_RFo

--- takeout_reserve/dishes/models.py
+++ takeout_reserve/dishes/models.py
@@ -15,6 +15,13 @@ class Dish(models.Model):
         return self.name
 
 class Order(models.Model):
+    """
+    status
+        0: 注文受付中
+        1: 注文受付完了
+        2: 決済完了
+        3: 受け取り完了
+    """    
     customer = models.CharField(max_length=50)
     ordered_at = models.DateTimeField(default=timezone.now)
     status = models.SmallIntegerField(default=0)
@@ -22,6 +29,6 @@ class Order(models.Model):
     def __str__(self):
         return f'{self.customer[:6]}:{self.ordered_at}'
 class OrderDetail(models.Model):
-    dish = models.ForeignKey(to=Dish, on_delete=models.DO_NOTHING)
+    dish = models.ForeignKey(to=Dish, null=True, on_delete=models.SET_NULL)
     amount = models.SmallIntegerField()
-    order = models.ForeignKey(to=Order, on_delete=models.DO_NOTHING)
+    order = models.ForeignKey(to=Order, on_delete=models.CASCADE)
--- takeout_reserve/takeout_reserve/urls.py
+++ takeout_reserve/takeout_reserve/urls.py
@@ -1,3 +1,7 @@
+import requests
+import json
+import traceback
+
 from django.contrib import admin
 from django.urls import path
 from django.http import HttpResponse
@@ -12,61 +16,80 @@ from linebot import LineBotApi
 from linebot.models import TextSendMessage, TemplateSendMessage, CarouselTemplate,CarouselColumn,PostbackAction,MessageAction
 from linebot.exceptions import LineBotApiError
 
-line_bot_api = LineBotApi('x1BuyT+Guw+lawys4jlznLgLOy70jmclbxBxxlaPfZJ7S/RD15NhV8kn5QCD1ujatJ3fPZlyPYgYguHWhsdpJ72Hdg9N7ElApJDaOzvC1FGS0EGjAKtPT39by8XzWtnUk4ru0Kmk2XSTsUPHm4kYrQdB04t89/1O/w1cDnyilFU=')
+ACCESS_TOKEN = 'x1BuyT+Guw+lawys4jlznLgLOy70jmclbxBxxlaPfZJ7S/RD15NhV8kn5QCD1ujatJ3fPZlyPYgYguHWhsdpJ72Hdg9N7ElApJDaOzvC1FGS0EGjAKtPT39by8XzWtnUk4ru0Kmk2XSTsUPHm4kYrQdB04t89/1O/w1cDnyilFU='
+line_bot_api = LineBotApi(ACCESS_TOKEN)
 
 @csrf_exempt
 def callback(request):
     sent_json = json.loads(request.body)
     sent_message = sent_json['events'][0]['message']['text']
     reply_token = sent_json['events'][0]['replyToken']
-    userId = sent_json['events'][0]['source']['userId']
+    user_id = sent_json['events'][0]['source']['userId']
 
-    if sent_message == 'メニュー':
-        categories = Category.objects.all()
-        columns = []
-        for category in categories:
-            actions = []
-            for dish in category.dish_set.all():
-                ma = MessageAction(
-                    label=dish.name,
-                    text=dish.name
+    headers = {'Authorization': f'Bearer {ACCESS_TOKEN}'}
+    result = requests.get(f'https://api.line.me/v2/bot/profile/{user_id}', headers=headers)
+    user_name = json.loads(result.text)['displayName']
+    try:
+        if sent_message == 'メニュー':
+            categories = Category.objects.all()
+            columns = []
+            for category in categories:
+                actions = []
+                for dish in category.dish_set.all():
+                    ma = MessageAction(
+                        label=dish.name,
+                        text=dish.name
+                    )
+                    actions.append(ma)
+                cm = CarouselColumn(
+                        thumbnail_image_url='https://1.bp.blogspot.com/-D2I7Z7-HLGU/Xlyf7OYUi8I/AAAAAAABXq4/jZ0035aDGiE5dP3WiYhlSqhhMgGy8p7zACNcBGAsYHQ/s1600/no_image_square.jpg',
+                        title=category.name,
+                        text='description1',
+                        actions=actions
+                    )
+                columns.append(cm)
+
+            message = TemplateSendMessage(
+                alt_text='Carousel template',
+                template=CarouselTemplate(
+                    columns=columns
                 )
-                actions.append(ma)
-            cm = CarouselColumn(
-                    thumbnail_image_url='https://1.bp.blogspot.com/-D2I7Z7-HLGU/Xlyf7OYUi8I/AAAAAAABXq4/jZ0035aDGiE5dP3WiYhlSqhhMgGy8p7zACNcBGAsYHQ/s1600/no_image_square.jpg',
-                    title=category.name,
-                    text='description1',
-                    actions=actions
-                )
-            columns.append(cm)
-
-
-        message = TemplateSendMessage(
-            alt_text='Carousel template',
-            template=CarouselTemplate(
-                columns=columns
             )
-        )
-    elif Dish.objects.filter(name=sent_message).first():
-        dish = Dish.objects.filter(name=sent_message).first()
-        order = Order.objects.filter(customer=userId).first()
-        if not order:
-            order = Order(customer=userId)
-            order.save()
-        order_detail = OrderDetail.objects.filter(order=order, dish__name=sent_message).first()
-        if order_detail:
-            order_detail.amount = order_detail.amount + 1
-        else:
-            order_detail = OrderDetail(order=order, dish=dish, amount=1)
-        order_detail.save()
+        # オーダー受付中のときのみ注文を受け付ける
+        elif Dish.objects.filter(name=sent_message).first():
+            dish = Dish.objects.filter(name=sent_message).first()
+            order = Order.objects.filter(customer=user_id, status=0).first()
+            if not order:
+                order = Order(customer=user_id)
+                order.save()
 
-        message = TextSendMessage(text='他にご注文はありますか？')
+            order_detail = OrderDetail.objects.filter(order=order, dish__name=sent_message).first()
+            if order_detail:
+                order_detail.amount = order_detail.amount + 1
+            else:
+                order_detail = OrderDetail(order=order, dish=dish, amount=1)
+            order_detail.save()
 
-    elif sent_message == '完了':
-        order = Order.objects.filter(customer=userId).first()
-        if order:
+            message = TextSendMessage(text='他にご注文はありますか？')
+        elif Order.objects.filter(customer=user_id, status=0).first()\
+            and sent_message == '完了':
+            order = Order.objects.filter(customer=user_id, status=0).first()
+            if not order:
+                # 新しいオーダーを作る
+                order = Order(customer=user_id)
+                order.save()
+
             order_details = order.orderdetail_set.all()
             message = TextSendMessage(text=f'{order.ordered_at:%Y年%m月%d日 %H時%M分}に注文を受け付けました。\n{output_order_details(order_details)}')
+            # 注文完了
+            order.status = 1
+            order.save()
+        else:
+            message = TextSendMessage(text=f'{user_name}さん いらっしゃいませ。\n「メニュー」と送信すると、注文を開始できます。')
+    except Exception:
+            message = TextSendMessage(text=f'{user_name}さん いらっしゃいませ。\n「メニュー」と送信すると、注文を開始できます。')
+    
+            traceback.print_exc()
     line_bot_api.reply_message(reply_token, message)
 
 def output_order_details(order_details):
